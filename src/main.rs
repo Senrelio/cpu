2f2bc29073c18a6ad39cbe7d032cddd8efd44974
songfanzhen <songfanzhen@gmail.com>
2022-04-13 21:02:17 +0800

    multiplier

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,64 @@
 fn main() {
     let mut cpu = CPU::new();
-    cpu.current_operation = 0x8014;
     cpu.registers[0] = 5;
     cpu.registers[1] = 10;
+    cpu.registers[2] = 10;
+    cpu.registers[3] = 10;
+    let mem = &mut cpu.memory;
+    mem[0] = 0x80;
+    mem[1] = 0x14;
+    mem[2] = 0x80;
+    mem[3] = 0x24;
+    mem[4] = 0x80;
+    mem[5] = 0x34;
     cpu.run();
+    assert_eq!(cpu.registers[0], 35);
 }
 
 pub struct CPU {
-    current_operation: u16,
-    registers: [u8; 2],
+    registers: [u8; 16],
+    position_in_memory: usize,
+    memory: [u8; 0x1000],
 }
 
 impl CPU {
     fn new() -> CPU {
         CPU {
-            current_operation: 0,
-            registers: [0, 0],
+            registers: [0; 16],
+            position_in_memory: 0,
+            memory: [0; 0x1000],
         }
     }
     fn read_opcode(&self) -> u16 {
-        self.current_operation
+        let idx_op = self.position_in_memory;
+        let op_l = self.memory[idx_op] as u16;
+        let op_r = self.memory[idx_op + 1] as u16;
+        op_l << 8 | op_r
     }
     fn run(&mut self) {
         loop {
             let opcode = self.read_opcode();
+            self.position_in_memory += 2;
             let c = ((opcode & 0xf000) >> 12) as u8;
             let x = ((opcode & 0x0f00) >> 8) as u8;
             let y = ((opcode & 0x00f0) >> 4) as u8;
             let d = (opcode & 0x000f) as u8;
             match (c, x, y, d) {
-                (8, _, _, 4) => self.add_xy(x, y),
+                (0, 0, 0, 0) => return,
+                (0x8, _, _, 0x4) => self.add_xy(x, y),
                 _ => todo!("opcode {:04x}", opcode),
             }
         }
     }
     fn add_xy(&mut self, x: u8, y: u8) {
-        self.registers[x as usize] += self.registers[y as usize];
+        let arg1 = self.registers[x as usize];
+        let arg2 = self.registers[y as usize];
+        let (val, overflow) = arg1.overflowing_add(arg2);
+        self.registers[x as usize] = val;
+        if overflow {
+            self.registers[0xf] = 1;
+        } else {
+            self.registers[0xf] = 0;
+        }
     }
 }
